c1de1924d99125e71dd354275da75c21a04cd4f6
Tryliom <eros67mc@gmail.com>
2017-09-26 16:02:55 +0200

    BowAimbot en cours

--- minecraft/neko/module/modules/combat/BowAimbot.java
+++ minecraft/neko/module/modules/combat/BowAimbot.java
@@ -5,13 +5,20 @@ import neko.module.Module;
 import neko.module.modules.hide.Friends;
 import neko.utils.Utils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemFood;
 
 public class BowAimbot extends Module {
-	private EntityPlayer currEn=null;
+	private EntityLivingBase currEn=null;
+	private Double fov=30.0;
+	// Plus proche/éloignée
+	// + de vie / - de vie
+	// Celui qui a le moins d'enchant/ le moins d'enchant projectile prot
+	
+	
 	
 	public BowAimbot() {
 		super("BowAimbot", -1, Category.COMBAT);
@@ -26,27 +33,57 @@ public class BowAimbot extends Module {
 	}
 	
 	public void onUpdate() {
-		if (!mc.thePlayer.isUsingItem())
+		if (!this.haveBow())
+			return;
+		boolean valid = false;
+		for(Object o : (var.mode.equalsIgnoreCase("Player") ? mc.theWorld.playerEntities : mc.theWorld.loadedEntityList)) {
+			if (o instanceof EntityLivingBase) {
+				EntityLivingBase en = (EntityLivingBase) o;           
+	            if (this.isValid(en)) {
+	            	if (currEn!=null && currEn==en)
+	            		valid=true;
+	            	else if (currEn==null)
+	            		valid=true;
+	            	currEn=en;
+	            	Utils.faceBowEntityClient(en);
+	            }
+			}
+        }
+		if (!valid)
 			currEn=null;
 	}
 	
-	public void onRightClick() {
-		for(Object o : mc.theWorld.playerEntities) {
-            EntityPlayer en = (EntityPlayer) o;
-               // test si il est à 30 de fov si oui viser lui
-            try {
-				Item i = mc.thePlayer.getCurrentEquippedItem().getItem();
-				if ((i instanceof ItemBlock) || ((i instanceof ItemFood) && mc.thePlayer.getFoodStats().needFood()))
-					return;
-			} catch (Exception e) {
-				return;
-			}
-            if(currEn==null ? Utils.isInFov(en, 30) : en==currEn && mc.thePlayer.isUsingItem() && Friends.isFriend(en.getName()) && mc.thePlayer!=en &&
-            		Item.getIdFromItem(mc.thePlayer.getCurrentEquippedItem().getItem())==261) {  
-            	currEn=en;
-            	Utils.faceBowEntityClient(en);
-            }
-        }
+	private boolean isValid(EntityLivingBase en) {
+		if (!this.haveBow())
+			return false;
+		if (!(currEn==null ? Utils.isInFov(en, fov) : en==currEn))
+			return false;
+		if (Friends.isFriend(en.getName()))
+			return false;
+		if (Utils.isPlayer(en))
+			return false;
+		if (Item.getIdFromItem(mc.thePlayer.getCurrentEquippedItem().getItem())!=261)
+			return false;
+		if (var.mode.equalsIgnoreCase("Mob") ? Utils.isPlayer(en) : false)
+			return false;
+		
+		
+		return true;
+	}
+	
+	private boolean haveBow() {
+		try {
+			Item i = mc.thePlayer.getCurrentEquippedItem().getItem();
+			if ((i instanceof ItemBlock) || ((i instanceof ItemFood) && mc.thePlayer.getFoodStats().needFood()))
+				return false;
+		} catch (Exception e) {
+			return false;
+		}
+		if (!mc.thePlayer.isUsingItem()) {
+			currEn=null;
+			return false;
+		}
+		return true;
 	}
 
 }
