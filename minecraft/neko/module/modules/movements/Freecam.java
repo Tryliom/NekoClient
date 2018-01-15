88d813efebde3d898b11d3ff5a7f02d5a0e470d8
Haldy Alexis <alexis.haldy@nagra.com>
2018-01-15 16:50:43 +0100

    Freecam changé et amélioré, à tester

--- minecraft/neko/module/modules/movements/Freecam.java
+++ minecraft/neko/module/modules/movements/Freecam.java
@@ -7,6 +7,7 @@ import neko.module.Module;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.minecraft.util.MathHelper;
+import net.minecraft.world.WorldSettings.GameType;
 
 public class Freecam extends Module {
 	Minecraft mc = Minecraft.getMinecraft();
@@ -41,39 +42,40 @@ public class Freecam extends Module {
 	}
 	
 	public void setValues() {
-		this.values = "§6Speed:§7 "+speed;
+		this.values = "ï¿½6Speed:ï¿½7 "+speed;
 	}
 	
 	public void onUpdate() {		
-		mc.thePlayer.noClip=true;
-		mc.thePlayer.motionX = 0.0D;
-	    mc.thePlayer.motionY = 0.0D;
-	    mc.thePlayer.motionZ = 0.0D;
-	    mc.thePlayer.onGround=false;
-	    mc.thePlayer.jumpMovementFactor = speed;
-	    if (mc.gameSettings.keyBindJump.pressed) {
-	    	if (mc.thePlayer.isCollidedVertically) {
-		    	  mc.thePlayer.setPosition(mc.thePlayer.posX, mc.thePlayer.posY+1, mc.thePlayer.posZ);
-		      } else
-	      mc.thePlayer.motionY += speed;
-	    }
-	    if (mc.gameSettings.keyBindSneak.pressed) {
-	      if (mc.thePlayer.isCollidedVertically) {
-	    	  mc.thePlayer.setPosition(mc.thePlayer.posX, mc.thePlayer.posY-1, mc.thePlayer.posZ);
-	      } else
-	    	mc.thePlayer.motionY -= speed;
-	    }
-	    if (mc.gameSettings.keyBindForward.pressed)
-	    {
-	      float var1 = mc.thePlayer.rotationYaw * 0.01745329F;
-	      mc.thePlayer.motionX -= MathHelper.sin(var1) * 0.5F;
-	      mc.thePlayer.motionZ += MathHelper.cos(var1) * 0.5F;
-	    }
-	    if (mc.gameSettings.keyBindBack.pressed)
-	    {
-	      float var1 = mc.thePlayer.rotationYaw * 0.01745329F;
-	      mc.thePlayer.motionX -= MathHelper.sin(var1) * -0.5F;
-	      mc.thePlayer.motionZ += MathHelper.cos(var1) * -0.5F;
-	    }
+		mc.thePlayer.setGameType(GameType.SPECTATOR);
+//		mc.thePlayer.noClip=true;
+//		mc.thePlayer.motionX = 0.0D;
+//	    mc.thePlayer.motionY = 0.0D;
+//	    mc.thePlayer.motionZ = 0.0D;
+//	    mc.thePlayer.onGround=false;
+//	    mc.thePlayer.jumpMovementFactor = speed;
+//	    if (mc.gameSettings.keyBindJump.pressed) {
+//	    	if (mc.thePlayer.isCollidedVertically) {
+//		    	  mc.thePlayer.setPosition(mc.thePlayer.posX, mc.thePlayer.posY+1, mc.thePlayer.posZ);
+//		      } else
+//	      mc.thePlayer.motionY += speed;
+//	    }
+//	    if (mc.gameSettings.keyBindSneak.pressed) {
+//	      if (mc.thePlayer.isCollidedVertically) {
+//	    	  mc.thePlayer.setPosition(mc.thePlayer.posX, mc.thePlayer.posY-1, mc.thePlayer.posZ);
+//	      } else
+//	    	mc.thePlayer.motionY -= speed;
+//	    }
+//	    if (mc.gameSettings.keyBindForward.pressed)
+//	    {
+//	      float var1 = mc.thePlayer.rotationYaw * 0.01745329F;
+//	      mc.thePlayer.motionX -= MathHelper.sin(var1) * 0.5F;
+//	      mc.thePlayer.motionZ += MathHelper.cos(var1) * 0.5F;
+//	    }
+//	    if (mc.gameSettings.keyBindBack.pressed)
+//	    {
+//	      float var1 = mc.thePlayer.rotationYaw * 0.01745329F;
+//	      mc.thePlayer.motionX -= MathHelper.sin(var1) * -0.5F;
+//	      mc.thePlayer.motionZ += MathHelper.cos(var1) * -0.5F;
+//	    }
 	}
 }
